63f63a560df82bc69f5af30b860dd527d61a008f
Josh Willigers <jwilligers@gmail.com>
2015-02-28 21:03:51 +1100

    Added Jacobs minecraft program

---
+++ python/minecraftExamples.py
@@ -0,0 +1,18 @@
+from mcpi import minecraft
+from time import sleep
+
+mc = minecraft.Minecraft.create()
+
+flower = 38
+
+while True:
+    x, y, z = mc.player.getPos()
+    mc.setBlock(x, y, z, flower)
+    sleep(0.1)
+	
+	
+
+# Make a cube of solid stone	
+stone = 1
+x, y, z = mc.player.getPos()
+mc.setBlocks(x+1, y+1, z+1, x+11, y+11, z+11, stone)
