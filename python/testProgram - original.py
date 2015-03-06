b94f1cace9c5a8a76bc6508d4189f5eb8e2b7faa
Josh Willigers <jwilligers@gmail.com>
2015-03-06 19:39:35 +1100

    Minecraft example

---
+++ python/testProgram - original.py
@@ -0,0 +1,52 @@
+import mcpi.minecraft as minecraft
+import time
+
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(3, GPIO.OUT)
+GPIO.setup(5, GPIO.OUT)
+GPIO.setup(11, GPIO.IN)
+
+
+mc = minecraft.Minecraft.create()
+
+mc.postToChat("Hello world")
+
+x,y,z = mc.player.getPos()
+
+print (x)
+print (y)
+print(z)
+
+# mc.player.setPos(x + 10, y ,z)
+x,y,z = mc.player.getPos()
+print (x)
+
+tnt = 46
+stone = 1
+dirt = 2
+gold = 41
+x1,y1,z1 = mc.player.getPos()
+
+while True:
+	x, y, z = mc.player.getPos()
+	if mc.getBlock(x, y-1, z) == dirt or mc.getBlock(x, y-1, z) == stone:
+		mc.setBlock(x, y-1, z, tnt,1)
+		
+	if (mc.getBlock(9, 7, -13) == 50):
+		GPIO.output(3, True)
+	else:
+		GPIO.output(3, False)
+
+	if (mc.getBlock(10, 7, -13) == 50):
+		GPIO.output(5, True)
+	else:
+		GPIO.output(5, False)
+	if (GPIO.input(11)):
+		mc.setBlock(x + 2, y + 2, z + 2, tnt, 1)
+		
+	
+	time.sleep(0.05)
+
+
