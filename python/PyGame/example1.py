fb0c7f2d59bb5dcf6cb924823192e32f05b41d08
Josh Willigers <jwilligers@gmail.com>
2015-03-07 09:03:26 +1100

    Made example more complex

--- python/PyGame/example1.py
+++ python/PyGame/example1.py
@@ -3,7 +3,11 @@ import pygame
 pygame.init()
 
 window = pygame.display.set_mode((500, 400))
+pygame.display.set_caption('Hello World!')
 
 while True:
-	pygame.draw.rect(window, (255,0,0), (0,0,50,30))
+	pygame.draw.rect(window, (255,0,0), (100, 100, 50, 50))
+	pygame.draw.rect(window, (0,255,0), (150, 100, 50, 50))
+	pygame.draw.rect(window, (0,0,255), (200, 100, 50, 50))
+	
 	pygame.display.update()
