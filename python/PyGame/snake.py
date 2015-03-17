a02028417bb73e1b89f764d0ca54c029e81277b4
Josh Willigers <jwilligers@gmail.com>
2015-03-17 21:04:27 +1100

    Crated game loop and window

--- python/PyGame/snake.py
+++ python/PyGame/snake.py
@@ -7,5 +7,15 @@ gameWindowY = 700
 
 blockSize = 15
 
+gameDisplay = pygame.display.set_mode((gameWindowX, gameWindowY))
+pygame.display.set_caption('Snake')
 
-def gameLoop():
+gameExit = False
+
+while not gameExit:
+    for event in pygame.event.get():
+        if event.type = pygame.QUIT:
+            gameExit = True
+
+pygame.quit()
+quit()
