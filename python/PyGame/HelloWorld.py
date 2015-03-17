b3021c3e3b3b728d1cd96273817236cd952ff270
Josh Willigers <jwilligers@gmail.com>
2015-03-17 20:43:41 +1100

    Removed line numbers

--- python/PyGame/HelloWorld.py
+++ python/PyGame/HelloWorld.py
@@ -1,12 +1,12 @@
 import pygame, sys
- 2. from pygame.locals import *
- 3.
- 4. pygame.init()
- 5. DISPLAYSURF = pygame.display.set_mode((400, 300))
- 6. pygame.display.set_caption('Hello World!')
- 7. while True: # main game loop
- 8.     for event in pygame.event.get():
- 9.         if event.type == QUIT:
-10.             pygame.quit()
-11.             sys.exit()
-12.     pygame.display.update()
+from pygame.locals import *
+
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((400, 300))
+pygame.display.set_caption('Hello World!')
+while True: # main game loop
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
