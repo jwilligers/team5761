737732e843a91fbbcad26bbae114697bca328689
Josh Willigers <jwilligers@gmail.com>
2015-04-19 15:52:59 +1000

    Images from kivy-game-tutorial

--- python/Kivy/test2.py
+++ python/Kivy/test2.py
@@ -1,18 +1,28 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.image import Image
 from kivy.graphics import Color, Rectangle
 from kivy.core.window import Window
+
+class Sprite(Image):
+	def __init__(self, **kwargs):
+			super(Sprite, self).__init__(**kwargs)
+			self.size = self.texture_size
+	
 
 class Game(Widget):
 	def __init__(self, **kwargs):
 		super(Game, self).__init__(**kwargs)
-		with self.canvas:
-			Color(.5, .5, 1.0)
-			Rectangle(pos=(0, 0), size = self.size)
-	
+		self.background = Sprite(source = 'images/background.png')
+		self.size = self.background.size
+		self.add_widget(self.background)
+		self.add_widget(Sprite(source = 'images/bird.png'))
+		
 class GameApp(App):
 	def build(self):
-		return Game(size=Window.size)
+		game = Game()
+		Window.size = game.size
+		return game
 		
 if __name__ == "__main__":
 	GameApp().run()
