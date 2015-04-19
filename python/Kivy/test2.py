8de19658a6b4c91f39ab05b964a75662ab715b34
Josh Willigers <jwilligers@gmail.com>
2015-04-19 15:19:16 +1000

    Changed to simple example 2 with blue background

--- python/Kivy/test2.py
+++ python/Kivy/test2.py
@@ -1,12 +1,18 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.graphics import Color, Rectangle
+from kivy.core.window import Window
 
 class Game(Widget):
-	pass
+	def __init__(self, **kwargs):
+		super(Game, self).__init__(**kwargs)
+		with self.canvas:
+			Color(.5, .5, 1.0)
+			Rectangle(pos=(0, 0), size = self.size)
 	
 class GameApp(App):
 	def build(self):
-		return Game()
+		return Game(size=Window.size)
 		
 if __name__ == "__main__":
 	GameApp().run()
