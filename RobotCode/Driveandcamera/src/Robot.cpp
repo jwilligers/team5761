cf868103f85c1afcd346514945993ad194b6e3ce
Josh Willigers <jwilligers@gmail.com>
2015-10-23 19:53:14 +1100

    Changes to robot program for DDU competition

--- RobotCode/Driveandcamera/src/Robot.cpp
+++ RobotCode/Driveandcamera/src/Robot.cpp
@@ -1,19 +1,21 @@
 #include "WPILib.h"
- 
+#include "NetworkTables/NetworkTable.h"
+#include <DriverStation.h>
+
 class RampedJoystick : public GenericHID
 {
 public:
 	RampedJoystick(GenericHID* joystick) :
-		                                 m_joystick(joystick)
+	 m_joystick(joystick)
 	{
 	}
  
 	virtual ~RampedJoystick()
 	{
 	}
- 
-	virtual float GetX(JoystickHand hand) { return -m_joystick->GetX(hand) * (-m_joystick->GetThrottle()+1)/2; }
-	virtual float GetY(JoystickHand hand) { return m_joystick->GetY(hand) * (-m_joystick->GetThrottle()+1)/2; }
+	//X is steering
+	virtual float GetX(JoystickHand hand) { return rampedInput(m_joystick->GetX(hand),0); }
+	virtual float GetY(JoystickHand hand) { return rampedInput(m_joystick->GetY(hand),1); }
 	virtual float GetZ() { return m_joystick->GetZ(); }
 	virtual float GetTwist() { return m_joystick->GetTwist(); }
 	virtual float GetThrottle() { return m_joystick->GetThrottle(); }
@@ -24,9 +26,26 @@ public:
 	virtual bool GetBumper(JoystickHand hand) { return m_joystick->GetBumper(hand); }
 	virtual bool GetRawButton(uint32_t button) { return m_joystick->GetRawButton(button); }
  
-	virtual int GetPOV(uint32_t pov) { return m_joystick->GetPOV(pov); }
+	virtual int GetPOV(uint32_t pov = 0) { return m_joystick->GetPOV(pov); }
+
  
 private:
+	float rampGradient() {
+		return (1 - m_joystick->GetThrottle()) / 2;
+	}
+/*	float rampedInput(float input) {
+		return (rampGradient() + input * input) * input;
+	}
+*/
+	float rampedInput(float input, bool xaxis)
+	{
+		if(xaxis == true)
+			return rampGradient() * input; //   was ((1 + input) / 2);
+		else if (xaxis == false && rampGradient() != 0)
+			return input * 0.5;
+		else
+			return 0;
+	}
 	GenericHID* m_joystick;
 };
  
@@ -39,19 +58,28 @@ class Robot: public IterativeRobot
 	Joystick stick; // only joystick
 	RampedJoystick rampedStick;
 
-	LiveWindow *lw;
-	Servo nerfServo;
-	int autoLoopCounter;
 
- 
+	LiveWindow *lw;
+	VictorSP drumMotor;
+	int autoLoopCounter;
+	int drumMultiplier;
+	float throttleMultiplier;
+
+
+	const int SingleDrumMultipier = 5;
+	const int DoubleDrumMultipier = 3;
+	const int TripleDrumMultipier = 4;
+	const int QuadDrumMultipier = 5;
+
 public:
 	Robot() :
 		myRobot(0, 1),	// these must be initialized in the same order
 		stick(0),		// as they are declared above.
 		rampedStick(&stick),
 		lw(NULL),
-		nerfServo(2),
-		autoLoopCounter(0)
+		drumMotor(2),
+		autoLoopCounter(0),
+		throttleMultiplier(1)
 	{
 		myRobot.SetExpiration(0.1);
 	}
@@ -96,14 +124,58 @@ private:
  
 	void TeleopInit()
 	{
- 
+		throttleMultiplier = 1;
+		drumMultiplier = 1;
+		SmartDashboard::PutString("test", "test");
 	}
  
 	void TeleopPeriodic()
 	{
 		myRobot.ArcadeDrive(rampedStick); // drive with arcade style (use right stick)
-		nerfServo.Set((rampedStick.GetTwist()+1)/2);
-		SmartDashboard.PutData("test", "test");
+
+		throttleMultiplier = rampedStick.GetThrottle();
+		SmartDashboard::PutNumber("Drum Multiplier", drumMultiplier);
+
+		if (rampedStick.GetRawButton(SingleDrumMultipier) == true)
+		{
+			drumMultiplier = 1;
+		}
+
+		if (rampedStick.GetRawButton(DoubleDrumMultipier) == true)
+		{
+			drumMultiplier = 2;
+		}
+
+		if (rampedStick.GetRawButton(TripleDrumMultipier) == true)
+		{
+			drumMultiplier = 3;
+		}
+
+		if (rampedStick.GetRawButton(QuadDrumMultipier) == true)
+		{
+			drumMultiplier = 4;
+		}
+
+		if (rampedStick.GetPOV() != -1)
+		{
+			if (rampedStick.GetPOV() != 180)
+			{
+				// down: drum down
+				drumMotor.Set(-0.05);
+			}
+			else
+			{
+				// up: drum up
+				drumMotor.Set(0.25*drumMultiplier);
+			}
+		}
+		else {
+			// not being operated
+			drumMotor.Set(0.1 * drumMultiplier - 0.1);
+
+		}
+		//nerfServo.Set((rampedStick.GetTwist()+1)/2);
+		//SmartDashboard.PutData("test", "test");
 	}
  
 	void TestPeriodic()
