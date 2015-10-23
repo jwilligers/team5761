3490727a841e5cabbd682de261b388335cc9e4c7
Josh Willigers <jwilligers@gmail.com>
2015-10-23 19:52:43 +1100

    Kivy Test

--- python/Kivy/test2.py
+++ python/Kivy/test2.py
@@ -3,20 +3,28 @@ from kivy.uix.widget import Widget
 from kivy.uix.image import Image
 from kivy.graphics import Color, Rectangle
 from kivy.core.window import Window
+from kivy.clock import Clock
 
 class Sprite(Image):
 	def __init__(self, **kwargs):
 			super(Sprite, self).__init__(**kwargs)
 			self.size = self.texture_size
-	
+
+class Background(Sprite):
+	def update(self):
+		self.x -= 2
 
 class Game(Widget):
 	def __init__(self, **kwargs):
 		super(Game, self).__init__(**kwargs)
-		self.background = Sprite(source = 'images/background.png')
+		self.background = Background(source = 'images/background.png')
 		self.size = self.background.size
 		self.add_widget(self.background)
 		self.add_widget(Sprite(source = 'images/bird.png'))
+		Clock.schedule_interval(self.update, 1.0/60.0)
+		
+	def update(self, *ignore):
+		self.background.update()
 		
 class GameApp(App):
 	def build(self):
